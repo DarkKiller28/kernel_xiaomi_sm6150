7726cec3cd19565e2deb8d2901cda4d388c8439a
Arun KS <arunks@codeaurora.org>
2018-12-28 00:34:24 -0800

    mm: convert zone->managed_pages to atomic variable
    totalram_pages, zone->managed_pages and totalhigh_pages updates are
    protected by managed_page_count_lock, but readers never care about it.
    Convert these variables to atomic to avoid readers potentially seeing a
    store tear.
    
    This patch converts zone->managed_pages.  Subsequent patches will convert
    totalram_panges, totalhigh_pages and eventually managed_page_count_lock
    will be removed.
    
    Main motivation was that managed_page_count_lock handling was complicating
    things.  It was discussed in length here,
    https://lore.kernel.org/patchwork/patch/995739/#1181785 So it seemes
    better to remove the lock and convert variables to atomic, with preventing
    poteintial store-to-read tearing as a bonus.
    
    Link: http://lkml.kernel.org/r/1542090790-21750-3-git-send-email-arunks@codeaurora.org
    Signed-off-by: Arun KS <arunks@codeaurora.org>
    Suggested-by: Michal Hocko <mhocko@suse.com>
    Suggested-by: Vlastimil Babka <vbabka@suse.cz>
    Reviewed-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Reviewed-by: Pavel Tatashin <pasha.tatashin@soleen.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    [YaroST12: resolved 4.14 conflicts.]
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    [fiqri19102002: Add LMK]
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Anush02198 <anush.4376@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -417,7 +417,7 @@ static int get_minfree_scalefactor(gfp_t gfp_mask)
 	unsigned long nr_usable = 0;
 
 	for_each_zone_zonelist(zone, z, zonelist, gfp_zone(gfp_mask))
-		nr_usable += zone->managed_pages;
+		nr_usable += zone_managed_pages(zone);
 
 	return max_t(int, 1, mult_frac(100, nr_usable, totalram_pages));
 }
