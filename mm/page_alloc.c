b70075c234195421c716afd7238c7dab8c227420
MiDoNaSR545 <57400341+MiDoNaSR545@users.noreply.github.com>
2026-05-02 19:38:04 +0700

    Revert "mm: Tweak memory pressure for 6/8GB RAM devices"
    This reverts commit 0d574a84e7a7a6a1505a1abdb1fd02effa2e1524.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -317,7 +317,7 @@ compound_page_dtor * const compound_page_dtors[] = {
 /*
  * Try to keep at least this much lowmem free.  Do not allow normal
  * allocations below this point, only high priority ones. Automatically
- * tuned according to the amount of memory in the system.P
+ * tuned according to the amount of memory in the system.
  */
 int min_free_kbytes = 1024;
 int user_min_free_kbytes = -1;
